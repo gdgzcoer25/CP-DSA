53a424a9e1c2200a4ca02ba0ac46362e6e6e473b
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2025-01-22 07:42:22 +0530

    Add files via upload

---
+++ 22-01-2025/22-01-2025/java/reverse and array.java
@@ -0,0 +1,25 @@
+//https://www.geeksforgeeks.org/problems/reverse-an-array/0
+
+public void reverseArray(int arr[]) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            // Swap the elements at left and right indices
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+//Test Cases
+// Input: arr = [1, 4, 3, 2, 6, 5]
+// Output: [5, 6, 2, 3, 4, 1]
+
+// Input: arr = [4, 5, 2]
+// Output: [2, 5, 4]
+
+// Input: arr = [1]
+// Output: [1]
