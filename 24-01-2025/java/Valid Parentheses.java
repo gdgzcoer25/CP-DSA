832c2175716d7c71c1cdad60122e78624b3e7e44
purvighatage <purvighatage@gmail.com>
2025-01-23 23:03:24 +0530

    Update Valid Parentheses.java

--- 24-01-2025/java/Valid Parentheses.java
+++ 24-01-2025/java/Valid Parentheses.java
@@ -1,6 +1,26 @@
 //https://leetcode.com/problems/valid-parentheses/description/
 
 //code 
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+           else if (stack.isEmpty() || 
+                     (c == ')' && stack.pop() != '(') || 
+                     (c == '}' && stack.pop() != '{') || 
+                     (c == ']' && stack.pop() != '[')) {
+                return false;
+            }
+        }
+    return stack.isEmpty();
+    }
+}
+
+
 
 // test cases
 // Example 1:
