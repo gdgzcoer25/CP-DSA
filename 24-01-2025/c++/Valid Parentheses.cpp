0687020066649f8c6b8c20e9643ef4dc5947bbb9
purvighatage <purvighatage@gmail.com>
2025-01-23 22:42:44 +0530

    Create Valid Parentheses.cpp

---
+++ 24-01-2025/c++/Valid Parentheses.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+
+//code
+
+
+// test cases
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
