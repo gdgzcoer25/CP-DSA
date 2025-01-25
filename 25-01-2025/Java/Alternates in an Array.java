34a82896820ed1b4a070a9ed116198ca82158606
Vinayak Jaywant Gawade <164622243+VinayakGawade009@users.noreply.github.com>
2025-01-25 20:12:23 +0530

    Update Alternates in an Array.java (i+=2)
    i+=2 for alternates

--- 25-01-2025/Java/Alternates in an Array.java
+++ 25-01-2025/Java/Alternates in an Array.java
@@ -4,10 +4,8 @@ import java.util.List;
 class Solution {
     public List<Integer> getAlternates(List<Integer> arr) {
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < arr.size(); i++) {
-            if (i % 2 == 0) {
-                ans.add(arr.get(i));
-            }
+        for (int i = 0; i < arr.size(); i += 2) {
+            ans.add(arr.get(i));
         }
         return ans;
     }
