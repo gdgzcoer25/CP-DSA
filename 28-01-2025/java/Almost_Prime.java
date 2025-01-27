9d2fe91a10c26c36dd028da566da72ea0be50921
purvighatage <purvighatage@gmail.com>
2025-01-28 00:01:43 +0530

    Update Almost_Prime.java

--- 28-01-2025/java/Almost_Prime.java
+++ 28-01-2025/java/Almost_Prime.java
@@ -1,1 +1,50 @@
+//https://codeforces.com/contest/26/problem/A
+code:-
+ import java.util.Scanner;
 
+public class AlmostPrimeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (countPrimeDivisors(i) == 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static int countPrimeDivisors(int num) {
+        int divisors = 0;
+
+        for (int i = 2; i <= num; i++) {
+            if (num % i == 0 && isPrime(i)) {
+                divisors++;
+            }
+        }
+
+        return divisors;
+    }
+
+    public static boolean isPrime(int num) {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return num > 1;
+    }
+}
+// Testcases:-
+//   Input
+// 10
+// Output
+// 2
+// Input
+// 21
+// Output
+// 8
