5f57bbc93f045a73eb254b25a85f418b005cc6c1
sakshi_hande03 <sakshihande537@gmail.com>
2025-01-27 23:15:33 +0530

    added

---
+++ 27-01-2025/Java/latword.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s.trim();
+        String words[] = s.split(" ");
+        String lastWord = words[words.length-1];
+        return lastWord.length();
+    }
+}
