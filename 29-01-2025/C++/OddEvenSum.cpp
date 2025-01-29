0b3c62d09752118853ee1ed245efb2088d0b3c53
Mohandinkar <mohandinkar75@gmail.com>
2025-01-29 22:43:57 +0530

    added

--- 29-01-2025/C++/OddEvenSum.cpp
+++ 29-01-2025/C++/OddEvenSum.cpp
@@ -1,24 +1,34 @@
 // https://www.geeksforgeeks.org/problems/sum-of-odd-and-even-elements3033/1?page=5&difficulty=School&sortBy=submissionsp
 
 // Code
-
 class Solution {
   public:
     vector<int> findSum(int n) {
         // Code here
-        int odd_sum = 0;
-        int even_sum = 0;
-        for(int i=1;i<=n;i++){
-            if(i%2==0) {
-                even_sum += i;
+        int odd = 0;
+        int even = 0;
+        vector<int>ans;
+        int i = 1;
+        while( i<=n){
+            if(i & 1){
+                odd +=i;
+                i++;
             }
-            else{ 
-                 odd_sum +=i;
-                }
+            else{
+               even += i; 
+               i++;
+            }
         }
-        return {odd_sum, even_sum};
+        
+        ans.push_back(odd);
+        ans.push_back(even);
+        
+        return ans;
+        
+        
     }
 };
+
 
 /*
                     TEST CASES
