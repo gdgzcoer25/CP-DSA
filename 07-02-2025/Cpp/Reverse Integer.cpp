c67d0daebc4fa1b6eb2e9a0ac75da829a7e2bd48
Rudra Pratap Singh <145354880+Rudra2637@users.noreply.github.com>
2025-02-06 11:47:31 +0000

    Added

---
+++ 07-02-2025/Cpp/Reverse Integer.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/reverse-integer/description/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans=0;
+        while(x!=0){
+            int digit=x%10;
+            if(INT_MIN/10>ans||ans>INT_MAX/10)
+            return 0;
+         
+            ans=(ans*10)+digit;
+            x=x/10;
+        }
+        return ans;
+        
+    }
+};
+// Example 1:
+
+// Input: x = 123
+// Output: 321
